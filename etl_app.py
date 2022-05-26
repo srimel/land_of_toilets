eb17d312140d6a40e783d941369be46e55f9be2a
srimel <stuart.rimel@gmail.com>
2022-05-26 13:34:22 -0700

    fixed several issues with attribute names and varchar size

--- etl_app.py
+++ etl_app.py
@@ -9,7 +9,7 @@ def main():
     response = input("Do you want to create all the tables in DB? [y/n]: ")
     if(response == 'y'):
         create_all_tables(conn)
-    response = input("Do you want to wrangle and insert all data into DB? [y/n]: ")
+    response = input("\nDo you want to wrangle and insert all data into DB? [y/n]: ")
     if(response == 'y'):
         wrangle_and_insert()
     print("Finished! Now check for correctness...")
