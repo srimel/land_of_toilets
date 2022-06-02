3e42e3be1a03a6a81193ebb1be4811d600a313da
srimel <stuart.rimel@gmail.com>
2022-06-01 21:51:29 -0700

    added some boilerplate code

--- query.py
+++ query.py
@@ -1,8 +1,17 @@
 import psycopg2
 from sqlalchemy import create_engine
+from db import *
 import os
 
 def example_query(db_conn):
     cur = db_conn.cursor()
-    cur.execute("SELECT * FROM toilets;")
-    print(cur.fetchall())   # <- what does this do?
+    cur.execute("SELECT * FROM toilets LIMIT 20;")
+    print(cur.fetchall())   # <- what does this do? 
+
+def run_all():
+    setup_env()
+    conn = create_connection() 
+    example_query(conn)
+
+if __name__ == "__main__":
+    run_all()
