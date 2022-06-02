82d0ee1bd30ec011bb978df82b43612df9296e2a
Peter Wells <unfinishedideas@gmail.com>
2022-06-01 22:52:36 -0700

    first 4 down

--- query.sql
+++ query.sql
@@ -1,8 +1,26 @@
 SELECT * FROM toilets LIMIT 20;
 --1. How many toilets in Cooma have parking?
+SELECT COUNT(*) FROM toilets
+JOIN location_rel USING(FacilityID) JOIN locations USING(LocID) JOIN town_rel USING(LocID)
+JOIN towns USING(TownID) JOIN access USING(facilityID)
+WHERE Town='Cooma' AND parking=True;
+
 --2. How many public toilets are there in Australia?
---3. What are all the sporting facility toilets that are also dump points?
+SELECT COUNT(*) FROM toilets;
+
+--3. What are all the sporting facility toilets that are also dump points? // REWORD: How many sporting facility toilets are also dump points?
+SELECT COUNT(*) FROM toilets 
+JOIN facility_rel USING(facilityID) JOIN facility_types FT USING(typeid) 
+JOIN dump_points USING (facilityID)
+WHERE FT.name='Sporting facility';
+
 --4. Which city has the most public toilets?
+SELECT town, COUNT(facilityID) AS number_o_bathrooms FROM
+location_rel JOIN locations USING(locID) JOIN town_rel USING(locID) 
+JOIN towns T USING(townID) 
+GROUP BY town ORDER BY town;
+
+
 --5. What percentage of toilets are open 24 hours a day?
 --6. How many toilets in WA require payment?
 --7. How many unisex toilets are in a park or reserve?
